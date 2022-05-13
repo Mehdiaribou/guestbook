ade1daa1b35ca522feee93c0e85afc624dc694b3
Mehdi Laribou <laribou@my.com>
2022-05-13 20:17:09 +0100

    Comments Form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,22 +3,27 @@
 namespace App\Controller;
 
 
+use Twig\Environment;
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ConferenceRepository;
-use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
     private $twig;
+    private $em;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig,EntityManagerInterface $em)
     {
         $this->twig = $twig;
+        $this->em = $em;
     }
     /**
      * @Route("/", name="homepage")
@@ -31,16 +36,27 @@ class ConferenceController extends AbstractController
     }
 
     /**
-    * @Route("/conference/{id}", name="conference")
+    * @Route("/conference/{slug}", name="conference")
     */
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setConference($conference);
+            $this->em->persist($comment);
+            $this->em->flush();
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]); 
+        }
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference,$offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
         'conference' => $conference,
         'comments' => $paginator,
+        'comment_form' => $form->createView(),
         'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
         'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),])); 
     }
--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,15 +2,20 @@
 
 namespace App\Entity;
 
-use App\Repository\ConferenceRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
-class Conference
+class Conference 
 {
     /**
      * @ORM\Id
@@ -39,6 +44,11 @@ class Conference
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -48,6 +58,19 @@ class Conference
     {
         return $this->id;
     }
+
+    public function __toString(): string
+    {
+        return $this->city.' '.$this->year;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
+    }
+
 
     public function getCity(): ?string
     {
@@ -114,8 +137,19 @@ class Conference
 
         return $this;
     }
-    public function toString(): string
+
+    public function getSlug(): ?string
     {
-        return $this->city.' '.$this->year;
+        return $this->slug;
     }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+   
+    
 }
